db727cb84cd9120ada96f4158259a49bde617d03
Dustin Haefele <Dustin.haefele@gmail.com>
2019-11-09 17:50:43 -0300

    changes to user search component

--- src/components/UserSearch/UserSearch.js
+++ src/components/UserSearch/UserSearch.js
@@ -4,7 +4,7 @@ import config from '../../config';
 export default class UserSearch extends React.Component {
 
   handleSearch = searchTerm => {
-    return fetch(`${config.API_ENDPOINT}/users/search/user_name`, {method: 'GET', body: JSON.stringify({searchTerm})})
+    return fetch(`${config.API_ENDPOINT}/users/user_name`, {method: 'GET', body: JSON.stringify({searchTerm})})
       .then(res => {
         if(!res.ok) {
           return res.json().then(e=> Promise.reject(e))
