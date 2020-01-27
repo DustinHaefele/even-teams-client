774c8d46b4035b2b117ae5d665813602160e54ba
Dustin Haefele <Dustin.haefele@gmail.com>
2020-01-27 16:01:25 -0500

    added button and select on list item

--- src/components/UserListItem/UserListItem.js
+++ src/components/UserListItem/UserListItem.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
-export default function UserListItem (props) {
-  
+export default function UserListItem(props) {
   return (
-    <li key={props.user.id}>
-      <h3>{props.user.user_name}</h3>
-      <span>: {props.user.full_name}</span>
+    <li>
+      <h3>
+        {props.user.user_name}
+        <span>: {props.user.full_name}</span>
+      </h3>
+      <select
+        required
+        className="player_skill"
+        id="player_skill"
+        name="player_skill"
+      >
+        <option value={1}>1 - worst</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option>
+        <option value={5}>5 - best</option>
+      </select>
+      <button type="submit" className="button first-button">
+            Add This User
+      </button>
+          {/* <button onClick={this.props.toggleForm} className="button">
+            Cancel
+          </button> */}
     </li>
-  )
+  );
 }
